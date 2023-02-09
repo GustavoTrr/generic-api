acac886176a4ceb5cf3c32cf4a162523592a047b
gustavotrr <gustavotrr@gmail.com>
2023-02-09 13:51:52 -0300

    Upgrading code to allow other HTTP Methods and to work with dynamic JSON

--- app.js
+++ app.js
@@ -1,25 +1,130 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
+const PORT = 3000;
 
-app.get('/minhafesta/convidados', (req, res) => {
-  const guests = [
-    {
-      id: 1,
-      name: 'John Doe',
-      cpf: '12345678901',
-      foods: ['Pizza', 'Hamburger', 'Salad']
-    },
-    {
-      id: 2,
-      name: 'Jane Doe',
-      cpf: '10987654321',
-      foods: ['Steak', 'Fish', 'Fruit']
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/*', (request, response) => {
+  const requestedPath = request.url.substr(1);
+  const filePath = `./json_files/${requestedPath}.json`;
+
+  fs.exists(filePath, exists => {
+    if (!exists) {
+      return response.status(404).send('Not Found');
     }
-  ];
 
-  res.json(guests);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        return response.status(500).send('Internal Server Error');
+      }
+
+      response.send(JSON.parse(data));
+    });
+  });
 });
 
-app.listen(3000, () => {
-  console.log('JSON server listening on port 3000');
+app.post('*', (request, response) => {
+
+  const requestedPath = request.path.substring(1);
+
+  const filePath = `./json_files/${requestedPath}.json`;
+  const fileDirPath = path.dirname(filePath);
+
+  fs.mkdir(fileDirPath, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send({ message: 'An error occurred while creating the directories' });
+    }
+
+    fs.writeFile(filePath, JSON.stringify(request.body), (err) => {
+      if (err) {
+        console.error(err);
+        response.status(500).send({ error: 'Failed to write file' });
+      } else {
+        response.status(201).send({ message: 'File created successfully' });
+      }
+    });
+  });
+});
+
+app.put('*', (request, response) => {
+  const requestedPath = request.path.substring(1);
+  const filePath = `./json_files/${requestedPath}.json`;
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error(err);
+      response.status(404).send({ error: 'File not found' });
+      // Unccomment the lines below and comment the two lines above if you want to try create the file as the entity sent
+      // fs.writeFile(path, JSON.stringify(data), (err) => {
+      //   if (err) {
+      //     res.status(500).send({
+      //       error: 'Error while saving the data',
+      //     });
+      //   } else {
+      //     res.send({
+      //       message: 'Data saved successfully',
+      //     });
+      //   }
+      // });
+    } else {
+      fs.writeFile(filePath, JSON.stringify(request.body), (err) => {
+        if (err) {
+          console.error(err);
+          response.status(500).send({ error: 'Failed to update file' });
+        } else {
+          response.status(200).send({ message: 'File updated successfully' });
+        }
+      });
+    }
+  });
+});
+
+app.patch('*', (request, response) => {
+  const requestedPath = request.path.substring(1);
+  const filePath = `./json_files/${requestedPath}.json`;
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error(err);
+      response.status(404).send({ error: 'File not found' });
+    } else {
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          console.error(err);
+          response.status(500).send({ error: 'Failed to read file' });
+        } else {
+          let fileData = JSON.parse(data);
+          fileData = { ...fileData, ...request.body };
+          fs.writeFile(filePath, JSON.stringify(fileData), (err) => {
+            if (err) {
+              console.error(err);
+              response.status(500).send({ error: 'Failed to update file' });
+            } else {
+              response.status(200).send({ message: 'File updated successfully' });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+app.delete('/*', (request, response) => {
+  const requestedPath = request.path.substring(1);
+  const filePath = `./json_files/${requestedPath}.json`;
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      response.status(404).send('File not found');
+    } else {
+      response.send('File deleted successfully');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`JSON server listening on port ${PORT}`);
 });
